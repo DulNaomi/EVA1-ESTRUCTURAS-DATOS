41bf28096ba57481b0de96823f4e65ed1695b13a
DulNaomi <143562926+DulNaomi@users.noreply.github.com>
2024-08-28 19:01:03 -0600

    Add files via upload

---
+++ EVA1_6_ARREGLOSS/src/eva1_6_arregloss/EVA1_6_ARREGLOSS.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_6_arregloss;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_6_ARREGLOSS {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int [10];
+        System.out.println(arreglo);
+        arreglo [0] = 100;
+        arreglo [9] = 1000;
+        System.out.println("arreglo [0] =" + arreglo[0]);
+        System.out.println("arreglo [0] =" + arreglo[9]);
+
+
+    }
+    
+}
