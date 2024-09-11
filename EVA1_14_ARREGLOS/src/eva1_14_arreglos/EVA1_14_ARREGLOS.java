67d3e458567315375cb46862b25f5f96946a8901
DulNaomi <143562926+DulNaomi@users.noreply.github.com>
2024-09-10 18:53:17 -0600

    Add files via upload

---
+++ EVA1_14_ARREGLOS/src/eva1_14_arreglos/EVA1_14_ARREGLOS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_14_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_14_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          int arreglo[][][][] matriz = new int [4][3][3][2];
+          for (int i = 0; i < arreglo.length; i++) {
+              for (int j = 0; j < arreglo[i].length; j++) {
+                  for (int k = 0; k < arreglo[i][j].length; k++) {
+                      for (int l = 0; l < arreglo[i][j][k]; l++) {
+                          System.out.println("[" + arreglo[i][j][k][l] + "]");
+                      }
+                      
+                  }
+                  
+              
+            
+        
+
+    
+        
+   
+
